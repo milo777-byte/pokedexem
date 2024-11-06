048e9ec808575e0abd98317d64b478b0a93bf40a
emiliomontaleza <emiliomontaleza@gmail.com>
2024-11-06 03:28:20 +0000

    modified:   src/app/Components/graph/graph.component.css 	modified:   src/app/Components/graph/graph.component.ts 	modified:   src/app/Components/pokedex-view/pokedex-view.component.css 	modified:   src/app/Components/pokedex/pokedex.component.css 	modified:   src/app/app.component.ts 	modified:   src/app/app.config.ts modified:   src/index.html

--- src/app/Components/graph/graph.component.ts
+++ src/app/Components/graph/graph.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-graph',
@@ -11,30 +12,42 @@ import { CommonModule } from '@angular/common';
 export class GraphComponent implements OnChanges {
   @Input() pokemonId: number = 1;
   idPokemon: string = '#001';
-  tipoPokemon: string = 'Fuego 🔥';
-  nombrePokemon: string = 'Dome eres el lov of my life';
+  tipoPokemon: string = 'Cargando...';
+  nombrePokemon: string = 'Cargando...';
   vidaPokemon: number = 100;
   ataquePokemon: number = 100;
   defensaPokemon: number = 100;
 
+  constructor(private http: HttpClient) {
+    this.getPokemonData(this.pokemonId);
+  }
+
   ngOnChanges(changes: SimpleChanges) {
     if (changes['pokemonId']) {
       this.idPokemon = '#' + this.pokemonId.toString().padStart(3, '0');
+      this.getPokemonData(this.pokemonId);
     }
   }
 
-  getHealthBarClass(): string {
-    if (this.vidaPokemon > 70) {
-      return 'health-high';
-    } else if (this.vidaPokemon > 30) {
-      return 'health-medium';
-    } else {
-      return 'health-low';
-    }
+  getPokemonData(id: number) {
+    this.http.get(`https://pokeapi.co/api/v2/pokemon/${id}`)
+      .subscribe({
+        next: (data: any) => {
+          this.nombrePokemon = this.capitalizeFirstLetter(data.name);
+          this.tipoPokemon = this.capitalizeFirstLetter(data.types[0].type.name);
+          this.vidaPokemon = data.stats[0].base_stat;
+          this.ataquePokemon = data.stats[1].base_stat;
+          this.defensaPokemon = data.stats[2].base_stat;
+        },
+        error: (error) => {
+          console.error('Error fetching Pokemon data:', error);
+          this.nombrePokemon = 'Error al cargar';
+        }
+      });
   }
 
-  bajarVida(cantidad: number): void {
-    this.vidaPokemon = Math.max(0, this.vidaPokemon - cantidad);
+  capitalizeFirstLetter(string: string): string {
+    return string.charAt(0).toUpperCase() + string.slice(1);
   }
 }
 
