540e81259ae2bd708027e1baf1b9d4101bfb846d
emiliomontaleza <emiliomontaleza@gmail.com>
2024-11-06 03:54:08 +0000

    modified:   src/app/Components/pokedex-view/pokedex-view.component.css 	modified:   src/app/Components/pokedex-view/pokedex-view.component.html 	modified:   src/app/Components/pokedex-view/pokedex-view.component.ts

--- src/app/Components/pokedex-view/pokedex-view.component.ts
+++ src/app/Components/pokedex-view/pokedex-view.component.ts
@@ -1,23 +1,40 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { PokedexComponent } from '../pokedex/pokedex.component';
 import { GraphComponent } from '../graph/graph.component';
 
 @Component({
   selector: 'app-pokedex-view',
   standalone: true,
-  imports: [PokedexComponent, GraphComponent],
+  imports: [PokedexComponent, GraphComponent, FormsModule],
   templateUrl: './pokedex-view.component.html',
   styleUrls: ['./pokedex-view.component.css']
 })
 export class PokedexViewComponent {
-  pokemonId: number = 1; // Cambiado a 1 para empezar desde el primer pokemon
-  pokemonImagen: string = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/' + this.pokemonId + '.png';
+  pokemonId: number = 1;
+  pokemonImagen: string = '';
+  searchId: number | null = null;
+
+  ngOnInit() {
+    this.updatePokemonImage();
+  }
 
   changePokemon(evento: number): void {
     this.pokemonId += evento;
-    if(this.pokemonId < 1){
-      this.pokemonId = 1; // Aseguramos que no baje de 1
+    if (this.pokemonId < 1) {
+      this.pokemonId = 1;
     }
-    this.pokemonImagen = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/' + this.pokemonId + '.png';
+    this.updatePokemonImage();
+  }
+
+  searchPokemon(): void {
+    if (this.searchId && this.searchId > 0) {
+      this.pokemonId = this.searchId;
+      this.updatePokemonImage();
+    }
+  }
+
+  private updatePokemonImage(): void {
+    this.pokemonImagen = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${this.pokemonId}.png`;
   }
 }
